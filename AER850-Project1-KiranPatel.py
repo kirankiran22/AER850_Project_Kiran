b71058a5f83974612ca0c179dd42c539e073cd04
kirankiran22 <kiran.patel@torontomu.ca>
2024-10-10 13:08:45 -0400

    Create AER850-Project1-KiranPatel.py

---
+++ AER850-Project1-KiranPatel.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# By: Kiran Patel
+# AER 850 - Introduction to Machine Learning 
+# Project 1 
+# 
+
+"""
+Created on Thu Oct 10 12:59:41 2024
+
+@author: kiranpatel
+"""
+
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
